9473e97a4e833639fd1af657de85fa5406effafb
brycehoecker <133408086+brycehoecker@users.noreply.github.com>
2023-07-20 20:08:38 -0700

    Update script.sh

--- required_files/script.sh
+++ required_files/script.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-# Ask for the GitHub token
-echo "Please enter your GitHub token:"
-read GITHUB_TOKEN
-
-# Use the GitHub token to clone the repos listed in github_checkout.list
-while read repo; do git clone "https://${GITHUB_TOKEN}@github.com/$repo.git"; done < github_checkout.list
+# Clone the repos listed in github_checkout.list
+while read repo_url; do git clone "$repo_url"; done < github_checkout.list
 rm github_checkout.list
 
+
+
+
+
+# USE WHEN/IF the github_checkout.list is formatted like 
+# username/repositoryname
+#
+## Clone the repos listed in github_checkout.list
+#while read repo; do git clone "https://github.com/$repo.git"; done < github_checkout.list
+#rm github_checkout.list
+#
 # Rest of your script...
+## Ask for the GitHub token
+#echo "Please enter your GitHub token:"
+#read GITHUB_TOKEN
+#
+## Use the GitHub token to clone the repos listed in github_checkout.list
+#while read repo; do git clone "https://${GITHUB_TOKEN}@github.com/$repo.git"; done < github_checkout.list
+#rm github_checkout.list
+#
+## Rest of your script...
+
